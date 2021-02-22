800b256c3fd2c28946d8791d53dcaf568f5aac49
emelanson <elijahmelanson@gmail.com>
2021-02-21 21:56:44 -0500

    creating creating API and base components for app.

---
+++ app/src/components/DirectoryContainer/DirectoryContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import API from "../../utils/API";
+
+const DirectoryContainer = (props) => {
+    return (<>
+        <h1>ITS THE APP!</h1>
+    </>
+
+    );
+};
+
+export default DirectoryContainer;
