f9b4a4f2c92334bfda8d3412f5780fbdf7b01683
emelanson <elijahmelanson@gmail.com>
2021-02-22 19:10:29 -0500

    Sort by first name working

--- app/src/components/DirectoryContainer/DirectoryContainer.js
+++ app/src/components/DirectoryContainer/DirectoryContainer.js
@@ -5,6 +5,12 @@ class DirectoryContainer extends Component {
 
     state = { results: [], searchResults: [] };
 
+    constructor(props) {
+        super(props);
+        this.sortAscendingFirstNames = this.sortAscendingFirstNames.bind(this);
+        this.sortDescendingFirstNames = this.sortDescendingFirstNames.bind(this);
+    }
+
     componentDidMount() {
         this.employeeRetrieve();
     }
@@ -29,6 +35,21 @@ class DirectoryContainer extends Component {
         this.setState({ searchResults: searchResults });
     }
 
+    sortAscendingFirstNames() {
+        console.log(this.state.results);
+        let sort = this.state.searchResults.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
+        this.setState({ searchResults: sort });
+    }
+
+    sortDescendingFirstNames() {
+        console.log(this.state.results);
+        let sort = this.state.searchResults.sort((a, b) => (a.name.first < b.name.first) ? 1 : -1)
+        this.setState({ searchResults: sort });
+    }
+
+
+
+
     render() {
         return (
             <div style={{
@@ -40,6 +61,9 @@ class DirectoryContainer extends Component {
                     <label for="name">Search by First Name: </label>
                     <input type="text" id="name" name="name" onChange={this.handleSearch} />
                 </form>
+                <label>Sort by First Name: </label>
+                <button onClick={this.sortAscendingFirstNames}>Ascending!</button>
+                <button onClick={this.sortDescendingFirstNames}>Descending!</button>
                 <>
                     <ResultsTable results={this.state.searchResults} />
                 </>
--- app/src/components/ResultsTable/ResultsTable.js
+++ app/src/components/ResultsTable/ResultsTable.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function ResultsTable(props) {
-    console.log("RESULTS TABLE PROPS", props);
     return (
         <table>
             <thead>
